e8f5e87fa8b2b3def8f9b40fa5f654c6eaa2efbe
Kimuntu <ngunga89@icloud.com>
2025-04-14 22:07:20 +0200

    Configuration Next.js pour GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
+  basePath: '/katiopa',
+  assetPrefix: '/katiopa/',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
